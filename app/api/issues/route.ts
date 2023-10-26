948f0dc2e51945107af25368bdda4b9a6af2c498
Suyash Shingare <59977948+suyashrs@users.noreply.github.com>
2023-10-26 11:26:13 -0700

    Build API for creating issues

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,5 +1,6 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { z } from 'zod';
+import prisma from "@/prisma/client";
 
 const createIssueSchema = z.object({
     title: z.string().min(1).max(255),
@@ -10,6 +11,12 @@ export async function POST(request:NextRequest) {
     const body = await request.json();
     const validation = createIssueSchema.safeParse(body);
     if(!validation.success){
-        return NextRequest.json(validation.error.errors, { status: 400});
+        return NextResponse.json(validation.error.errors, { status: 400});
     }
+
+    const newIssue = await prisma.issue.create({
+        data: { title: body.title, description: body.description }
+    })
+ 
+    return NextResponse.json(newIssue, { status: 201 });
 }
