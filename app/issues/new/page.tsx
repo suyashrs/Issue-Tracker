8147280426fc51b8392f7f795de89e6662e49770
Suyash Shingare <59977948+suyashrs@users.noreply.github.com>
2024-01-07 15:34:18 -0800

    Redo Issue Model

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,15 +2,27 @@
 import React from 'react'
 import { Button, TextField } from '@radix-ui/themes';
 import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from 'react-hook-form';
 import "easymde/dist/easymde.min.css";
 
+interface IssueForm {
+    title: String,
+    description: String
+}
 const NewIssuePage = () => {
+    const { register, control } = useForm<IssueForm>();
+    
     return (
         <div className='max-w-xl space-y-3'>
             <TextField.Root>
-                <TextField.Input placeholder='Title' />
+                <TextField.Input placeholder='Title' {...register('title')}/>
             </TextField.Root>
-            <SimpleMDE placeholder='Description'/>
+            <Controller 
+                name="description"
+                control={control}
+                render={ ({ field }) =>  <SimpleMDE placeholder='Description' {...field} /> }
+            />
+           
             <Button>Submit New Issue</Button>
         </div>
     )
